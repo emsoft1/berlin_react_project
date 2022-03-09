91221fdf85fee5c6cf125c915c721a89e29da3fe
Ido Mandelman <idomand@gmail.com>
2022-03-09 11:43:05 +0100

    Added a React project with Three.js + created a basic Scene with animation

---
+++ src/Components/Scene.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useRef } from "react";
+import * as THREE from "three";
+
+export default function Scene() {
+  const mountRef = useRef(null);
+
+  useEffect(() => {
+    const mountRefCurrent = mountRef.current;
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+    const renderer = new THREE.WebGLRenderer();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    mountRefCurrent.appendChild(renderer.domElement);
+
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(geometry, material);
+
+    scene.add(cube);
+    camera.position.z = 5;
+
+    const animate = function () {
+      requestAnimationFrame(animate);
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+      renderer.render(scene, camera);
+    };
+    let onWindowResize = function () {
+      camera.aspect = window.innerWidth / window.innerHeight;
+      camera.updateProjectionMatrix();
+      renderer.setSize(window.innerWidth, window.innerHeight);
+    };
+
+    window.addEventListener("resize", onWindowResize, false);
+
+    animate();
+
+    return () => mountRefCurrent.removeChild(renderer.domElement);
+  }, []);
+
+  return <div ref={mountRef}></div>;
+}
