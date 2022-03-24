b69fa795da0be829f7147b512bbafb3b8e550f74
Ido Mandelman <idomand@gmail.com>
2022-03-24 18:38:03 +0100

    basic merge

--- src/util/index.js
+++ src/util/index.js
@@ -1,4 +1,4 @@
-import * as THREE from '../node_modules/three/build/three.module.js'
+import * as THREE from 'three/build/three.module'
 import initSVGObject from './paths.js'
 
 function d3threeD(exports) {
