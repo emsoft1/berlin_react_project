e187babdf38e7f7def81670a252d38ac2a3459d7
MzMaXaM <55605842+MzMaXaM@users.noreply.github.com>
2022-05-23 19:10:24 +0100

    we are now using json as a source of countrties

--- src/Components/ShapesParent.jsx
+++ src/Components/ShapesParent.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import Shapes from './Shapes.jsx'
-import countries from '../Util/countriesInfo.js'
 
 export default function ShapesParrent(props) {
   let countriesOnMap = []
   const showName=(data)=> {
     props.onNewName(data)
   }
+  let countries = require('../Util/countriesInfo.js').default
   const max = countries.length
   for (let i = 0; i < max; i++) {
       countriesOnMap.push(
         <Shapes 
         key={i} 
         color={countries[i].color}
-        depth={countries[i].population/8_000_000}
+        depth={2}
         country={countries[i].svg}
         name={countries[i].name}
         isMember={countries[i].euMember}
